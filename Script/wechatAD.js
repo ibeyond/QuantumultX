14b9f2a88ff042100e6eda6deaf2eeb019799c2e
iEwha <royiewha@gmail.com>
2021-07-05 19:11:42 +0800

    +

--- Script/wechatAD.js
+++ Script/wechatAD.js
@@ -1,12 +1,28 @@
-let resp = $response.body;
-try {
-    resp = JSON.parse(resp);
-    resp.advertisement_num=0;
-    resp.advertisement_info=[];
-    delete resp.appid;
-    resp = JSON.stringify(resp);
-} catch (e) {
-    console.log(`weixin ad error:\n${e.message}`);
-}
+/*
+微信 去除公众号文章底部广告
 
-$done({body: resp});
+***************************
+QuantumultX:
+
+[rewrite_local]
+^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
+
+[mitm]
+hostname = mp.weixin.qq.com
+
+***************************
+Surge4 or Loon:
+
+[Script]
+http-response ^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
+
+[MITM]
+hostname = mp.weixin.qq.com
+
+**************************/
+
+var obj = JSON.parse($response.body);
+obj.advertisement_num = 0;
+obj.advertisement_info = [];
+delete obj.appid;
+$done({body: JSON.stringify(obj)}); 
